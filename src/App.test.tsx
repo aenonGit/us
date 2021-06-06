9d75a653c1df2f59501fba769019b885d7976c95
Vincenzo Esposito <aenon.esposito@gmail.com>
2021-06-06 20:13:50 +0200

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App lang={'en'} />);
+  const linkElement = screen.getByText(/Enzo/i);
   expect(linkElement).toBeInTheDocument();
 });
